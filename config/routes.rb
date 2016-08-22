7c445eedcd73916ab1f0ff4eca1cc30fb4b78e59
Vivek Patel <vivek.patel@procore.com>
2016-08-21 22:48:22 -0700

    Added Facebook login button to users index page

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
 
   get 'users/index'
 
-  root 'users#new'
+  root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
