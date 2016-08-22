87cfd740ca3b3d424bd3ab75f745fdd65afd54a2
Vivek Patel <vivek.patel@procore.com>
2016-08-21 21:30:06 -0700

    Added annotations to latest models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Comment < ApplicationRecord
 end
