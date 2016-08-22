0b39d1fae2031d4a93c6bd4f98fce80b989a1b77
Vivek Patel <vivek.patel@procore.com>
2016-08-21 21:25:56 -0700

    Pushing migrations for posts and wishlists...

--- db/migrate/20160816060639_create_posts.rb
+++ db/migrate/20160816060639_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
+      t.string :title
       t.string :description
 
       t.timestamps
--- db/migrate/20160820230837_create_wishlists.rb
+++ db/migrate/20160820230837_create_wishlists.rb
@@ -1,6 +1,8 @@
 class CreateWishlists < ActiveRecord::Migration[5.0]
   def change
     create_table :wishlists do |t|
+      t.string :name
+      t.string :description
 
       t.timestamps
     end
