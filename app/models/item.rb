d67aaba7435b18cabad15d042ff2189575498c69
Vivek Patel <vivek.patel@procore.com>
2016-09-02 17:12:57 -0700

    Annotated item and user models along with test files

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Item < ApplicationRecord
   has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord 
   has_and_belongs_to_many :items
 end
